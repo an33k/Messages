f6aab5a953b9284bbbc0361d9275bc0a55fd15fe
an33k <an33k@users.noreply.github.com>
2019-12-12 02:56:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/messages/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.example.messages;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    private EditText mLoginEmail;
+    private EditText mLoginPassword;
+
+    private Button mLogin_btn;
+
+    private ProgressDialog mLoginProgress;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.login_toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mLoginProgress = new ProgressDialog(this);
+
+        mLoginEmail = (EditText) findViewById(R.id.login_email);
+        mLoginPassword = (EditText) findViewById(R.id.login_password);
+
+        mLogin_btn = (Button) findViewById(R.id.login_btn);
+
+        mLogin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mLoginEmail.getText().toString();
+                String password = mLoginPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+
+                    mLoginProgress.setTitle("Logging in");
+                    mLoginProgress.setMessage("Please wait while we are verifying.");
+                    mLoginProgress.setCanceledOnTouchOutside(false);
+                    mLoginProgress.show();
+                    loginUser(email, password);
+
+
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            mLoginProgress.dismiss();
+
+                            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+
+
+
+
+    }
+
+}
